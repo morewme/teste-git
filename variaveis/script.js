df329e187d1dfb408d85a88c27256366ce27d600
Bianca Mari <105242354+morewme@users.noreply.github.com>
2024-10-30 18:09:29 -0300

    Add files via upload

---
+++ variaveis/script.js
@@ -0,0 +1,11 @@
+// Retorne o url da página atual utilizando o objeto window
+console.log(location.href);
+
+// Seleciona o primeiro elemento da página que
+// possua a classe ativo
+const select = document.querySelector('.ativo');
+console.log(select);
+
+// Retorne a linguagem do navegador
+
+// Retorne a largura da janela 
